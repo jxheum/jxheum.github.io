67c2aeb2a42de72c752ada9d73e99d4823ba4b66
jjiheumi <jimmybaek1212@gmail.com>
2024-10-20 23:46:45 +0900

    asdf

---
+++ index_mui.js
@@ -0,0 +1,4 @@
+import '@material/web/divider/divider.js';
+        import '@material/web/icon/icon.js';
+        import '@material/web/list/list.js';
+        import '@material/web/list/list-item.js';
